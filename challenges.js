6801c2684aa33f665b6db6172bcd0c8f1c8c4183
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-06 20:59:01 -0700

    add question 1

--- challenges.js
+++ challenges.js
@@ -60,6 +60,11 @@ func checkDriverAge(age) {
 
 // 1. Create an object that has properties "username" and "password". Fill those values in with strings.
 
+let object = {
+    username: "Chase",
+    password: 123
+}
+
 // 2. Create an array which contains the object you have made above and name the array "database".
 
 // 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
