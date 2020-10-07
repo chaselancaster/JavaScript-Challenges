8664255c63199d91e7c42dbafc7ba433b81e557c
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-06 20:15:28 -0700

    add bonus answer

--- challenges.js
+++ challenges.js
@@ -19,13 +19,38 @@ if (Number(age) < 18) {
 // checkDriverAge(92);
 // it returns "Powering On. Enjoy the ride!"
 
+// 1.
 func checkDriverAge() {
-    let age = prompt("What is your age?")
-    return age
+    var age = prompt("What is your age?");
+    if (Number(age) < 18) {
+    alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+    alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
 }
 
+// 2.
 let checkDriverAge2 = function() {
-    let age = prompt("What is your age?")
-    return age
+    var age = prompt("What is your age?");
+    if (Number(age) < 18) {
+    alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+    alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+// BONUS
+func checkDriverAge(age) {
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+      } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+      } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+      }
 }
 
