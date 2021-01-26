49a53931e4ba9714a124778aec9f4a4f67a5815b
Chase Lancaster <chaseblancaster@gmail.com>
2021-01-25 20:28:13 -0800

    add maximum length difference problem

--- challenges.js
+++ challenges.js
@@ -464,3 +464,35 @@ function adjacentElementsProduct(inputArray) {
   biggestNum = Math.max(...arrayOfSums)
   return biggestNum
 }
+
+
+
+// Code Wars - Maximum Length Difference
+// https://www.codewars.com/kata/5663f5305102699bad000056/train/javascript
+
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+// Find max(abs(length(x) − length(y)))
+
+// If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+// Ex: a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+// a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+// mxdiflg(a1, a2) --> 13
+
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) {
+      return -1
+  }
+  let max = 0
+  for (let i = 0; i < a1.length; i++) {
+    for (let j = 0; j < a2.length; j++) {
+      let x = a1[i].length
+      let y = a2[j].length
+      if (Math.abs(x - y) > max) {
+        max = Math.abs(x - y)
+      }
+    }
+  }
+return max
+}
