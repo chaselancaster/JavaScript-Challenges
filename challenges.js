b8fe63df0abb5d4a2a34d960689480aab8e3b01f
Chase Lancaster <chaseblancaster@gmail.com>
2021-01-26 18:13:27 -0800

    add countSmileys problem

--- challenges.js
+++ challenges.js
@@ -496,3 +496,40 @@ function mxdiflg(a1, a2) {
   }
 return max
 }
+
+
+
+// Code Wars - Count the smiley faces!
+// https://www.codewars.com/kata/583203e6eb35d7980400002a/train/javascript
+
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+// Ex:
+// countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+function countSmileys(arr) {
+  const smileys = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"];
+  let count = 0;
+  
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < smileys.length; j++){
+      if (arr[i]===smileys[j]){
+        count++;
+      }
+    }
+    }
+  return count;
+  }
+
