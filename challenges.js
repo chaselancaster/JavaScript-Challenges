99585c7abe64b763992bc31307c72d15e8332c63
Chase Lancaster <chaseblancaster@gmail.com>
2021-02-04 17:34:02 -0800

    add minimum problem from eloquent javascript

--- challenges.js
+++ challenges.js
@@ -794,3 +794,17 @@ const interpret = function(command) {
   let finalString = parsedStringArray.join('')
   return finalString
 };
+
+
+
+
+// Eloquent JavaScript - Minimum
+// The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+
+function min(num1, num2) {
+	if (num1 < num2) {
+      return num1
+    } else {
+      return num2
+    }
+}
