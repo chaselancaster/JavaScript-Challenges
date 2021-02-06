ae5de8002938d0153e66214a38e8d575fc0e841f
Chase Lancaster <chaseblancaster@gmail.com>
2021-02-06 14:46:23 -0800

    add shapeArea code signal problem

--- challenges.js
+++ challenges.js
@@ -832,3 +832,22 @@ let isEven = (num) => {
    }
 }
 
+
+
+// Code Signal - shapeArea
+// https://app.codesignal.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ
+
+// Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+
+// A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+
+function shapeArea(n) {
+  let area = 1
+  for (let i = 1; i < n; i++) {
+      area += i * 4
+  }
+  return area
+}
+
+
+
