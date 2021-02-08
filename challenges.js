bd74c8a404be9c166b313e5995fc037abe80f319
Chase Lancaster <chaseblancaster@gmail.com>
2021-02-08 14:52:40 -0800

    add Make Array Consecutive 2 challenge

--- challenges.js
+++ challenges.js
@@ -851,3 +851,32 @@ function shapeArea(n) {
 
 
 
+// Code Signal - Make Array Consecutive 2
+// https://app.codesignal.com/arcade/intro/level-2/bq2XnSr5kbHqpHGJC
+
+// Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+// Ex:
+// For statues = [6, 2, 3, 8], the output should be
+// makeArrayConsecutive2(statues) = 3.
+
+// Ratiorg needs statues of sizes 4, 5 and 7.
+
+function makeArrayConsecutive2(statues) {
+  // sort the statues
+  statues.sort((a, b) => {
+      return a-b
+  })
+  // set min #
+  let min = statues[0]
+  // set max #
+  let max = statues[statues.length - 1]
+  // set count variable 
+  let count = 0
+  for (let i = min; i < max; i++) {
+      if (statues.indexOf(i) === -1) {
+          count++
+      }
+  }
+  return count
+}
