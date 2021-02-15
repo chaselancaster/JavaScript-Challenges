809a0f179a20164aab59ee9006067e735c4f4af3
Chase Lancaster <chaseblancaster@gmail.com>
2021-02-15 13:02:44 -0800

    add isLucky solution

--- challenges.js
+++ challenges.js
@@ -1159,3 +1159,36 @@ function commonCharacterCount(s1, s2) {
   
   return total
 }
+
+
+
+// Code Signal - isLucky
+// https://app.codesignal.com/arcade/intro/level-3/3AdBC97QNuhF6RwsQ
+
+// Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+
+// Given a ticket number n, determine if it's lucky or not.
+
+// Example
+
+// For n = 1230, the output should be
+// isLucky(n) = true;
+// For n = 239017, the output should be
+// isLucky(n) = false.
+
+function isLucky(n) {
+  let splitNum = n.toString().split('')
+  let firstSide = 0
+  let secondSide = 0
+  for (let i = 0; i < splitNum.length / 2; i++) {
+      firstSide += parseInt(splitNum[i])
+  }
+  for (let j = splitNum.length / 2; j < splitNum.length; j++) {
+      secondSide += parseInt(splitNum[j])
+  }
+  if (firstSide === secondSide) {
+      return true
+  } else {
+      return false
+  }
+}
