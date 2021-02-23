e82d688aac28d034536b27fb7cbe8c9a68961cc7
Chase Lancaster <chaseblancaster@gmail.com>
2021-02-22 19:44:30 -0800

    add sort by height solution

--- challenges.js
+++ challenges.js
@@ -1192,3 +1192,38 @@ function isLucky(n) {
       return false
   }
 }
+
+
+
+// Code Signal - Sort by Height
+// https://app.codesignal.com/arcade/intro/level-3/D6qmdBL2NYz49XHwM
+
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees. People can be very tall!
+
+// Example
+
+// For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+// sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
+
+function sortByHeight(a) {
+  let array2 = a
+  
+  array2 = array2.filter((element) => {
+      if (element !== -1) {
+          return element
+      }
+  }).sort((a, b) => {
+      return a-b
+  })
+  
+  let indexVal = 0
+  
+  for (let i = 0; i < a.length; i++) {
+      if (a[i] !== -1) {
+          a[i] = array2[indexVal]
+          indexVal++
+      }
+  }
+  
+  return a
+}
