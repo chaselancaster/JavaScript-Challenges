aa1c10b8d673f233e47ed2ab871dd2ed9c5fbdb4
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-01 14:39:14 -0800

    add code signal arrayChange solution

--- challenges.js
+++ challenges.js
@@ -1423,3 +1423,27 @@ function areSimilar(a, b) {
       return false
   }
 }
+
+
+
+// Code Signal - arrayChange
+// https://app.codesignal.com/arcade/intro/level-4/xvkRbxYkdHdHNCKjg 
+
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+
+// Example
+
+// For inputArray = [1, 1, 1], the output should be
+// arrayChange(inputArray) = 3.
+
+function arrayChange(inputArray) {
+  let count = 0
+  for (let i = 0; i < inputArray.length - 1; i++) {
+      if (inputArray[i] >= inputArray[i + 1]) {
+          let difference = inputArray[i] + 1 - inputArray[i + 1]
+          inputArray[i + 1] = inputArray[i] + 1
+          count += difference
+      }
+  }
+  return count
+ }
