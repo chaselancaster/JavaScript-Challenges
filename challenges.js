5f9c481999dc0a9c7364ea51198c97d9072b233b
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-01 14:49:59 -0800

    add palindromeRearranging solution

--- challenges.js
+++ challenges.js
@@ -1447,3 +1447,36 @@ function arrayChange(inputArray) {
   }
   return count
  }
+
+
+
+// Code Signal - palindromeRearranging
+// https://app.codesignal.com/arcade/intro/level-4/Xfeo7r9SBSpo3Wico
+
+// Given a string, find out if its characters can be rearranged to form a palindrome.
+
+// Example
+
+// For inputString = "aabb", the output should be
+// palindromeRearranging(inputString) = true.
+
+// We can rearrange "aabb" to make "abba", which is a palindrome.
+
+function palindromeRearranging(inputString) {
+  let charArray = inputString.split('')
+  let letterObject = {}
+  for (let i = 0; i < charArray.length; i++) {
+      if (!letterObject.hasOwnProperty(charArray[i])) {
+          letterObject[charArray[i]] = 1
+      } else {
+          letterObject[charArray[i]]++
+      }
+  }
+  let oddCount = 0
+  for (val in letterObject) {
+      if (letterObject[val] % 2 !== 0) {
+          oddCount++
+      }
+  }
+  return oddCount > 1 ? false : true
+}
