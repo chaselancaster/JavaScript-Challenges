8e61ec4e5ede7d5713968d4b804ee074b9a0db6f
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-03 13:52:56 -0800

    add areEquallyStrong solution

--- challenges.js
+++ challenges.js
@@ -1480,3 +1480,35 @@ function palindromeRearranging(inputString) {
   }
   return oddCount > 1 ? false : true
 }
+
+
+
+// Code Signal - areEquallyStrong
+
+// Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+// Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+
+// Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+// Example
+
+// For yourLeft = 10, yourRight = 15, friendsLeft = 15, and friendsRight = 10, the output should be
+// areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+// For yourLeft = 15, yourRight = 10, friendsLeft = 15, and friendsRight = 10, the output should be
+// areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+// For yourLeft = 15, yourRight = 10, friendsLeft = 15, and friendsRight = 9, the output should be
+// areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = false.
+
+function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
+  let yourWeakest = (yourLeft <= yourRight) ? yourLeft : yourRight
+  let yourStrongest = (yourLeft >= yourRight) ? yourLeft : yourRight
+  let friendsWeakest = (friendsLeft <= friendsRight) ? friendsLeft : friendsRight
+  let friendsStrongest = (friendsRight >= friendsLeft) ? friendsRight: friendsLeft
+  
+  if (yourStrongest === friendsStrongest && yourWeakest === friendsWeakest) {
+      return true
+  } else {
+      return false
+  }
+}
