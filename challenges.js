ffca70164a418c4cbd3b44ae266c1e78db57ea40
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-03 14:36:18 -0800

    add arrayMaximalAdjacentDifference solution

--- challenges.js
+++ challenges.js
@@ -1512,3 +1512,24 @@ function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
       return false
   }
 }
+
+
+
+// Code Signal - arrayMaximalAdjacentDifference
+// https://app.codesignal.com/arcade/intro/level-5/EEJxjQ7oo7C5wAGjE
+
+// Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+
+// Example
+
+// For inputArray = [2, 4, 1, 0], the output should be
+// arrayMaximalAdjacentDifference(inputArray) = 3.
+
+function arrayMaximalAdjacentDifference(inputArray) {
+  let maxDifference = 0
+  for (let i = 0; i < inputArray.length - 1; i++) {
+      let difference = Math.abs(inputArray[i] - inputArray[i + 1])
+      maxDifference = Math.max(maxDifference, difference)
+  }
+  return maxDifference
+}
