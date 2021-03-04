772ce4c9a376cd3ce7d4cfe0c9c51abd60a81832
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-04 14:25:27 -0800

    add isIPv4Address code signal solution

--- challenges.js
+++ challenges.js
@@ -1533,3 +1533,66 @@ function arrayMaximalAdjacentDifference(inputArray) {
   }
   return maxDifference
 }
+
+
+
+// Code Signal - isIPv4Address
+// https://app.codesignal.com/arcade/intro/level-5/veW5xJednTy4qcjso
+
+// An IP address is a numerical label assigned to each device (e.g., computer, printer) participating in a computer network that uses the Internet Protocol for communication. There are two versions of the Internet protocol, and thus two versions of addresses. One of them is the IPv4 address.
+
+// Given a string, find out if it satisfies the IPv4 address naming rules.
+
+// Example
+
+// For inputString = "172.16.254.1", the output should be
+// isIPv4Address(inputString) = true;
+
+// For inputString = "172.316.254.1", the output should be
+// isIPv4Address(inputString) = false.
+
+// 316 is not in range [0, 255].
+
+// For inputString = ".254.255.0", the output should be
+// isIPv4Address(inputString) = false.
+
+// There is no first number.
+
+function isIPv4Address(inputString) {
+  splitStr = inputString.split('.')
+  if (splitStr.length !== 4) {
+      return false
+  }
+  let trueCounter = 0
+  for (let i = 0; i < splitStr.length; i++) {
+      console.log(splitStr[i])
+      if (Number(splitStr[i]) >= 0 && Number(splitStr[i]) <= 255 && splitStr[i].length >= 1) {
+          trueCounter++
+      }
+  }
+  
+   if (trueCounter !== 4) {
+      return false
+  }
+  
+  let zeroCounter = 0
+  for (let j = 0; j < splitStr.length; j++) {
+      let splitIndex = []
+      if (splitStr[j].length > 1) {
+                  splitIndex = splitStr[j].split('')
+      }
+
+      for (let k = 0; k < splitIndex.length; k++) {
+          console.log(splitIndex[k])
+          if (Number(splitIndex[k]) === 0 || Number(splitIndex[k - 1]) === 0) {
+              zeroCounter++
+          }
+          // console.log(zeroCounter)
+      }
+      if (zeroCounter > 1) {
+          return false
+      }
+  }
+  
+  return true
+}
