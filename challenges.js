e4ff0ba3f9c46997bc3c73c3948e117124d78a33
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-10 15:41:32 -0800

    add count items matching a rule leetcode solution

--- challenges.js
+++ challenges.js
@@ -1628,3 +1628,43 @@ function avoidObstacles(inputArray) {
       }
   }
 }
+
+
+
+// Leetcode - Count Items Matching a Rule
+// https://leetcode.com/problems/count-items-matching-a-rule/
+
+// You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+// The ith item is said to match the rule if one of the following is true:
+
+// ruleKey == "type" and ruleValue == typei.
+// ruleKey == "color" and ruleValue == colori.
+// ruleKey == "name" and ruleValue == namei.
+// Return the number of items that match the given rule.
+
+ 
+
+// Example 1:
+
+// Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+// Output: 1
+// Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+
+const countMatches = function(items, ruleKey, ruleValue) {
+  let itemsMatched = 0
+  let keyIndex = 0
+  if (ruleKey === 'type') {
+      keyIndex = 0
+  } else if (ruleKey === 'color') {
+      keyIndex = 1
+  } else {
+      keyIndex = 2
+  }
+  for (let i = 0; i < items.length; i++) {
+      if (items[i][keyIndex] === ruleValue) {
+          itemsMatched++
+      }
+  }
+  return itemsMatched
+};
