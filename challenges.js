780de04a6643740fbe79cc31ead077a9f76dbcb9
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-11 20:36:39 -0800

    add create target array in the given order leet code solution

--- challenges.js
+++ challenges.js
@@ -1743,3 +1743,41 @@ const balancedStringSplit = function(s) {
   }
   return count
 };
+
+
+
+// Leet Code - Create Target Array in the Given Order
+// https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+// Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+// Initially target array is empty.
+// From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+// Repeat the previous step until there are no elements to read in nums and index.
+// Return the target array.
+
+// It is guaranteed that the insertion operations will be valid.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+// Output: [0,4,1,3,2]
+// Explanation:
+// nums       index     target
+// 0            0        [0]
+// 1            1        [0,1]
+// 2            2        [0,1,2]
+// 3            2        [0,1,3,2]
+// 4            1        [0,4,1,3,2]
+
+const createTargetArray = function(nums, index) {
+  // create empty target array
+  let target = []
+  // iterate over the nums array using with the index being the value of the elements in the index array
+  for (let i = 0; i < nums.length; i++) {
+     target.splice(index[i], 0, nums[i])
+  }
+  return target
+};
