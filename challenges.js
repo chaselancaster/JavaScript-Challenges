e662640d343de7bcb19c7c3e9ab650332e120d5c
Chase Lancaster <chaseblancaster@gmail.com>
2021-03-12 20:08:28 -0800

    add check if two string arrays are equivalent leet code solution

--- challenges.js
+++ challenges.js
@@ -1781,3 +1781,39 @@ const createTargetArray = function(nums, index) {
   }
   return target
 };
+
+
+
+// Leet Code - Check If Two String Arrays are Equivalent
+// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+// Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+// A string is represented by an array if the array elements concatenated in order forms the string.
+
+ 
+
+// Example 1:
+
+// Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+// Output: true
+// Explanation:
+// word1 represents string "ab" + "c" -> "abc"
+// word2 represents string "a" + "bc" -> "abc"
+// The strings are the same, so return true.
+
+const arrayStringsAreEqual = function(word1, word2) {
+  let concat1 = ''
+  let concat2 = ''
+  for (let i = 0; i < word1.length; i++) {
+      concat1 += word1[i]
+  }
+  for (let j = 0; j < word2.length; j++) {
+      concat2 += word2[j]
+  }
+  if (concat1 === concat2) {
+      return true
+  } else {
+      return false
+  }
+};
